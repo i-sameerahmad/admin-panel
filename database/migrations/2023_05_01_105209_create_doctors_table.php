2d29be8c611f163cb4f933a3bcc732b633e16421
ahmar_manzoor <B-24522@student.usa.edu.pk>
2023-05-06 03:23:36 -0700

    ahmar changes

--- database/migrations/2023_05_01_105209_create_doctors_table.php
+++ database/migrations/2023_05_01_105209_create_doctors_table.php
@@ -15,12 +15,12 @@ return new class extends Migration
     {
         Schema::create('doctors', function (Blueprint $table) {
             $table->id();
-            $table->doc_name();
-            $table->doc_description();
-            $table->doc__phone();
-            $table->doc_email();
-            $table->doc_address();
-            $table->doc_status();
+            $table->String('doc-name');
+            $table->String('doc-description');
+            $table->String('doc-phone');
+            $table->String('doc-email');
+            $table->String('doc-address');
+            $table->String('doc-status');
             $table->timestamps();
         });
     }
